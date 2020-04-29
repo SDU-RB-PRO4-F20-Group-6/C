cf4fec8a214b629bdeb1d7725ced7affbe46dc9a
thom9258 <49146152+thom9258@users.noreply.github.com>
2020-04-29 14:11:27 +0200

    Add files via upload

---
+++ SPI/inc/calibration.h
@@ -0,0 +1,39 @@
+/*****************************************************************************
+* University of Southern Denmark
+* Embedded Programming (EMP)
+*
+* MODULENAME.: 
+*
+* PROJECT....: 
+*
+* DESCRIPTION: 
+*
+* Change Log:
+******************************************************************************
+See calibration.c
+*****************************************************************************/
+
+#ifndef calibration
+#define calibration
+
+/***************************** Include files *******************************/
+#include <stdint.h>
+#include <stdbool.h>
+/*****************************    Defines    *******************************/
+
+/********************** External declaration of Variables ******************/
+
+/*****************************   Constants   *******************************/
+
+/*************************  Function interfaces ****************************/
+
+
+extern void calibratesetup(void);
+/*****************************************************************************
+*   Input    : -
+*   Output   : -
+*   Function : calibration for the frames to make sure they start at a predefined start position
+******************************************************************************/
+
+/****************************** End Of Module *******************************/
+#endif
